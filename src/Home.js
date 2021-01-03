6ed40230db65c529d8d68e9cf9c1aad1de532db4
John <john1997@live.ca>
2021-01-03 05:10:36 -0500

    Completed first draft of home page

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,11 @@ import { HashRouter as Router, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row } from 'react-bootstrap';
 import temp from './images/temp.png';
+import sponsorlogo from './images/sponsorlogo.png';
+import { FiInstagram  } from 'react-icons/fi';
+import { GrFacebookOption,GrSpotify  } from 'react-icons/gr';
+import { SiTiktok  } from 'react-icons/si';
+
 
 class Home extends Component {
     constructor(props) {
@@ -36,15 +41,49 @@ class Home extends Component {
                             ASIAN FEDERATION OF CHARITABLE ASIAN STUDENTS
                         </div>
                         <div class="img-content">
-                        <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Quisque faucibus nunc sit amet dui pretium porttitor. Duis nec dignissim purus, at sodales neque. Aenean dolor nisl, imperdiet commodo tincidunt ac, sodales in nisi. Duis commodo turpis vitae augue ultrices, eget luctus justo facilisis. Integer vitae lacus vitae libero lacinia laoreet. Maecenas at massa ut arcu accumsan viverra. Aliquam tincidunt augue lacus, at ullamcorper odio condimentum nec. Quisque malesuada porta imperdiet. Aliquam erat volutpat. Nunc condimentum justo sed pretium porta. Fusce.
+                            <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Quisque faucibus nunc sit amet dui pretium porttitor. Duis nec dignissim purus, at sodales neque. Aenean dolor nisl, imperdiet commodo tincidunt ac, sodales in nisi. Duis commodo turpis vitae augue ultrices, eget luctus justo facilisis. Integer vitae lacus vitae libero lacinia laoreet. Maecenas at massa ut arcu accumsan viverra. Aliquam tincidunt augue lacus, at ullamcorper odio condimentum nec. Quisque malesuada porta imperdiet. Aliquam erat volutpat. Nunc condimentum justo sed pretium porta. Fusce.
                         </div>
                     </div>
                 </div>
                 <div class="home-sponsors">
-                    testa
+                    <div class="sponsor-title">
+                        Our Sponsors
+                    </div>
+                    <div class="sponsor-center">
+                        <div class="sponsor-logooo">
+                            <img class="img-sponsor" src={sponsorlogo} /> <br />
+                        FUCK AJIO
+                        </div>
+                        <div class="sponsor-logooo">
+                            <img class="img-sponsor" src={sponsorlogo} /> <br />
+                        FUCK AJIO
+                        </div>
+                        <div class="sponsor-logooo">
+                            <img class="img-sponsor" src={sponsorlogo} /> <br />
+                        FUCK AJIO
+                        </div>
+                        <div class="sponsor-logooo">
+                            <img class="img-sponsor" src={sponsorlogo} /> <br />
+                            FUCK AJIO
+                        </div>
+                        <div class="sponsor-logooo">
+                            <img class="img-sponsor" src={sponsorlogo} /> <br />
+                            FUCK AJIO
+                        </div>
+                    </div>
+                    <div class="sponsor-button">
+                        <button class="sponsor-click">
+                            BECOME A SPONSOR
+                        </button>
+                    </div>
                 </div>
                 <div class="home-footer">
-                    testa
+                    <div class="footer-email">
+                        focus@mcmaster.ca
+                        <div class="footer-logos">
+                        <FiInstagram class="footer-icon"/> <GrFacebookOption class="footer-icon"/> <GrSpotify class="footer-icon"/> <SiTiktok class="footer-icon"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
